84bb1d0ca3b23b6c76732fdc8353beaca6de13c9
nayaracalenzo <queiroz.14@hotmail.com>
2026-04-29 11:45:42 -0300

    feat section

---
+++ src/components/Section.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+import seta from "../assets/Line.svg"
+
+const Section = ({ title = "Coleções em Destaque", children, link, textCenter }) => {
+  return (
+    <>
+      <section>
+        <div className="flex justify-between">
+          <h1 className={`${textCenter && "m-auto"} text-2xl font-bold text-[#474747]`}>{title}</h1>
+          {link && 
+          <Link className="text-[#c92071] w-30 text-nowrap font-normal flex items-center gap-2.5 text-[18px] " to={"produtos"}>
+           <span>Ver todos</span>  <img className="w-4" src={seta} alt="seta" />
+          </Link>}
+        </div>
+        {children}
+      </section>
+    </>
+  )
+}
+
+export default Section
