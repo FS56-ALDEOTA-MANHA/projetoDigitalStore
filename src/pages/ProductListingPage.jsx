9211ed97b7287ff2916a46894ab203e4013cee56
nayaracalenzo <queiroz.14@hotmail.com>
2026-05-13 11:58:09 -0300

    uso do json-server

--- src/pages/ProductListingPage.jsx
+++ src/pages/ProductListingPage.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react"
 
 const ProductListingPage = () => {
+  const [dados, setDados] = useState([])
+  async function fetchData() {
+    try {
+      const response = await fetch("http://localhost:3000/produtos")
+      const dados = await response.json()
+      console.log(dados)
+      setDados(dados)
+    } catch (error) {
+      console.error("Erro na requisição", error)
+    }
+  }
+
+  fetchData()
+
   return (
     <div>
-      <h1>Pagina de Lista de Produtos</h1>
+      {dados.map((produto) => (
+        <div>
+          {/* <img src="" alt="" /> */}
+          <div>
+            <span>Tênis</span>
+            <h2>{produto.name}</h2>
+            <span>{produto.price}</span>
+            {produto.priceDiscount !== produto.price && <span>{produto.priceDiscount}</span>}
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
