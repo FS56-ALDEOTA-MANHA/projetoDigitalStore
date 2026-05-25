1cf9971cb18035f093a8c256081fc80ab7e2aac8
Felipe9810 <felipe.marinho.lima@hotmail.com>
2026-05-25 15:27:45 -0300

    ajustes

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,5 @@
 const ProductCard = ({imagem, nome, preco, precoDesconto,}) => (
     <div className="w-[292px]">
-      {/* Área da imagem */}
       <div className="w-[292px] h-[321px] bg-white rounded-md flex items-center justify-center overflow-hidden">
         <img
           src={imagem}
@@ -8,15 +7,10 @@ const ProductCard = ({imagem, nome, preco, precoDesconto,}) => (
           className="w-full h-full object-cover"
         />
       </div>
-
-      {/* Nome do produto */}
       <h3 className="mt-3 text-base text-[#474747] font-normal">
         {name}
       </h3>
-
-      {/* Área de preços */}
       <div className="flex items-center gap-2 mt-1">
-        {/* Se existir desconto */}
         {precoDesconto ? (
           <>
             <span className="text-[#8F8F8F] text-2xl line-through">
@@ -28,7 +22,6 @@ const ProductCard = ({imagem, nome, preco, precoDesconto,}) => (
             </span>
           </>
         ) : (
-          /* Se NÃO existir desconto */
           <span className="text-[#1F1F1F] text-2xl font-bold">
             ${preco}
           </span>
@@ -37,4 +30,4 @@ const ProductCard = ({imagem, nome, preco, precoDesconto,}) => (
     </div>
   )
 
-export default ProductCard
+export default ProductCard;
