c707faa0dfa3697f1f1482640fb0451c18fe6948
Battyst <samuelbatistam3@gmail.com>
2026-05-26 13:43:22 -0300

    adicionei componente FilterGroup

--- src/pages/ProductListingPage.jsx
+++ src/pages/ProductListingPage.jsx
@@ -1,8 +1,54 @@
+import FilterGroup from "../components/FilterGroup"
 
 const ProductListingPage = () => {
   return (
-    <div>
-      <h1>Pagina de Lista de Produtos</h1>
+    <div className="bg-(--bg-page) min-h-screen p-10">
+
+      {/* Caixa lateral */}
+      <aside className="bg-white w-[308px] p-6 rounded-sm">
+
+        {/* Título */}
+        <h2 className="text-[16px] text-(--dark-gray-2) font-bold">
+          Filtrar por
+        </h2>
+
+        {/* Linha */}
+        <div className="w-full h-[1px] bg-(--light-gray-2) my-5"></div>
+
+        {/* Grupo Marca */}
+        <FilterGroup
+          title="Marca"
+          inputType="checkbox"
+          options={[
+            { text: "Adidas", value: "adidas" },
+            { text: "Nike", value: "nike" },
+            { text: "Puma", value: "puma" },
+          ]}
+        />
+
+        {/* Grupo Categoria */}
+        <FilterGroup
+          title="Categoria"
+          inputType="checkbox"
+          options={[
+            { text: "Esporte e lazer", value: "esporte" },
+            { text: "Casual", value: "casual" },
+            { text: "Corrida", value: "corrida" },
+          ]}
+        />
+
+        {/* Grupo Estado */}
+        <FilterGroup
+          title="Estado"
+          inputType="radio"
+          options={[
+            { text: "Novo", value: "novo" },
+            { text: "Usado", value: "usado" },
+          ]}
+        />
+
+      </aside>
+
     </div>
   )
 }
